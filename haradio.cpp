f2273ce979f481d051e1a8bec0e5dd3c0c0c07ef
Ryan Voots <simcop2387@simcop2387.info>
2013-03-17 13:38:08 -0700

    Adding some functions for SNAP protocol sending and receiving

--- haradio.cpp
+++ haradio.cpp
@@ -8,6 +8,162 @@
 #include <SPI.h>
 #include "haradio.h"
 
+struct packet {
+  uint8_t sync; // do i need this? probably not but it's not a bad idea
+  uint8_t HDB2;
+  uint8_t HDB1;
+  uint32_t target_address;
+  uint32_t source_address;
+  
+  uint8_t *data;
+  uint8_t dlength;
+  uint8_t rlength;  // real length, for protocol
+  // uint32_t pflags // protocol flags?  Do I want this?
+};
+
+int HARadio::setup_packet(struct packet *p) {
+  p->sync = 0b01010100; // required sync word
+  p->source_address = my_address;
+  uint8_t dlength = p->dlength;
+  
+  p->HDB2 = ((p->target_address & 0x00FF0000 ? 3 :   // Calculate how many bytes we need to use for the address, 3 for 24bit
+              p->target_address & 0x0000FF00 ? 2 :   // 2 for 16 bit, 1 for 8 bit, or 0 for broadcast!
+              p->target_address & 0x000000FF ? 1 : 0) << 6) |
+            ((p->source_address & 0x00FF0000 ? 3 :   // Do the same for the source address, but always have at least one byte of it, no sending from 0, yet
+	      p->source_address & 0x0000FF00 ? 2 : 1) << 4) | // TODO once I have this working I might use the command bit and protocol-bufs to handle internal details, in which case i'll do automatic address assignment, which will mean I want to be able to send from 0, or i'll use 255!
+	    ((0) << 2)  | // Protocol specific flags, not defined in standard yet, but reserved for future use
+	    (0)           // no ACKs needed, I won't be resending packets yet.  not enough ram on the uCs to do that well
+	    ;
+  p->HDB1 = (0 << 7) | // Command mode bit, we won't be using yet
+            (0 << 4) | // Error detection/correct mode, none, TODO implement CRC32;
+	    (dlength >= 0  && dlength <= 8  ? dlength :
+	     dlength >  8  && dlength <= 16 ? 0b1001 :
+     	     dlength >  16 && dlength <= 32 ? 0b1010 :
+     	     dlength >  32 && dlength <= 64 ? 0b1011 : 0b1111 // use 0b1111 as an error, since I can't send more than 64 bytes right now
+	    );
+
+  p->rlength = (dlength >= 0  && dlength <= 8  ? dlength :
+	        dlength >  8  && dlength <= 16 ? 16 :
+     	        dlength >  16 && dlength <= 32 ? 32 :
+     	        dlength >  32 && dlength <= 64 ? 64 : 0xFF // use 0xFF as an error, since I can't send more than 64 bytes right now
+	       );
+  
+  return p->rlength;
+}
+
+int HARadio::receive_packet(struct packet *p, uint8_t *buffer) {
+  int p_buff = 0;
+  uint8_t dab, sab, ndb;
+  
+  p->sync = buffer[p_buff++];
+  if (buffer[0] != 0b01010100)
+    return -1;
+  
+  p->HDB2 = buffer[p_buff++];
+  if ((p->HDB2 & 0b00110000 == 0x00) // Check for invalid source address bits for our network.  
+   && (p->HDB2 & 0b00001100 != 0x00) // Does it have protocol flags? if so, throw it out
+   && (p->HDB2 & 0b00000011 != 0x00)) // We don't support ACK right now, throw them out
+  return -2;
+  
+  p->HDB1 = buffer[p_buff++];
+  if ((p->HDB1 & 0b10000000) // Do we have command bit, we shouldn't
+   && (p->HDB1 & 0b01110000) // Do we have error detection? we shouldn't
+   && (p->HDB1 & 0b00001111 == 0xF) // Did we get a packet with user defined length? ignore it!
+  return -3;
+  
+  ndb = p->HDB1 & 0b00001111;
+  p->rlength = (ndb <= 0b1000 ? ndb :
+                ndb == 0b1001 ? 16  :
+                ndb == 0b1010 ? 32  : 64
+  );
+  
+  dab = (p->HDB2 & 0b11000000) >> 6;
+  if (dab == 0)
+    p->target_address = 0; // Broadcast packet!
+  else if (dab == 1) { // one byte
+    p->target_address = buffer[p_buff];
+  } else if (dab == 2) { // two bytes
+    p->target_address = buffer[p_buff] << 8 |
+                        buffer[p_buff+1];
+  } else { // three bytes! wowza!
+    p->target_address = buffer[p_buff]   << 16 |
+                        buffer[p_buff+1] << 8  |
+                        buffer[p_buff+2];
+  }
+  
+  p_buff += dab;
+  if (!(p->target_address == 0 ||
+        p->target_address != my_address))
+  return 0;  // not an error but not our packet
+  
+  sab = (p->HDB2 & 0b00110000) >> 4;
+  if (sab == 1) { // Same deal as DAB above, pull off the bytes as needed, but SAB can't be empty
+    p->source_address = buffer[p_buff];
+  } else if (sab == 2) {
+    p->source_address = buffer[p_buff] << 8 |
+                        buffer[p_buff+1];
+  } else {
+    p->source_address = buffer[p_buff]   << 16 |
+                        buffer[p_buff+1] << 8  |
+                        buffer[p_buff+2];
+  }
+  
+  p_buff += sab;
+  
+  // no such thing as an invalid source address right now, might have to have them later?
+  // At this point we have the entire packet header parsed, we now have a pointer to the data, what do we do now?
+  return p_buff;  // Give back the position in the buffer, so that the data can be read
+}
+
+int HARadio::send_packet(struct packet *p) {
+  uint8_t buffer[64 + 9]; // buffer, 64 bytes for data, and 9 for header.  TODO will actually fail when sending 64 bytes!
+  uint16_t p_buff = 0;
+  
+  if (p->sync != 0b01010100)
+    return -1; // don't send if it doesn't have the sync byte!
+  
+  buffer[p_buff++] = p->sync;
+  buffer[p_buff++] = p->HDB2;
+  buffer[p_buff++] = p->HDB1;
+  
+  
+  if (p->target_address & 0x00FF0000) {
+    buffer[p_buff++] = (p->target_address & 0xFF0000) >> 16;
+    buffer[p_buff++] = (p->target_address & 0x00FF00) >> 8;
+    buffer[p_buff++] = (p->target_address & 0x0000FF);
+  } else if (p->target_address & 0x0000FF00) {
+    buffer[p_buff++] = (p->target_address & 0x00FF00) >> 8;
+    buffer[p_buff++] = (p->target_address & 0x0000FF);
+  } else if (p->target_address & 0x000000FF) {
+    buffer[p_buff++] = (p->target_address & 0xFF);
+  } else {
+    // Broadcast address, no bits to send
+  }
+  
+  if (p->source_address & 0x00FF0000) {
+    buffer[p_buff++] = (p->source_address & 0xFF0000) >> 16;
+    buffer[p_buff++] = (p->source_address & 0x00FF00) >> 8;
+    buffer[p_buff++] = (p->source_address & 0x0000FF);
+  } else if (p->source_address & 0x0000FF00) {
+    buffer[p_buff++] = (p->source_address & 0x00FF00) >> 8;
+    buffer[p_buff++] = (p->source_address & 0x0000FF);
+  } else if (p->source_address & 0x000000FF) {
+    buffer[p_buff++] = (p->source_address & 0xFF);
+  } else {
+    // Broadcast address, no bits to send
+  }
+  
+  // copy the data into the buffer
+  for (int rl = p->rlength - 1; rl >= 0; rl--)
+    buffer[p_buff + rl] = p->data[rl];
+  
+  p_buff += p->rlength;
+  
+  
+  // Do the send!
+  txBuffer(buffer, p_buff);
+}
+
 HARadio::HARadio() {
 };
 int csn = 10;
@@ -71,47 +227,34 @@ void HARadio::reset()
 
     // Table-driven initialization
     const static prog_uchar init[] PROGMEM = {
-        1, 0x30,            // SRES        Soft reset strobe
-        2, 0x0B, 0x0A,      // FSCTRL1     IF frequency of 253.9 kHz
-        2, 0x0C, 0x00,      // FSCTRL0     No frequency offset (default)
-        2, 0x0D, 0x5D,      // FREQ2       FREQ = 0x5d13b1 = 2420 MHz
-        2, 0x0E, 0x13,      // FREQ1
-        2, 0x0F, 0xB1,      // FREQ0
-        2, 0x10, 0x2D,      // MDMCFG4     CHANBW = 541.666 kHz
-        2, 0x11, 0x3B,      // MDMCFG3     DRATE = 249.94 kBaud
-        2, 0x12, 0x73,      // MDMCFG2     MSK modulation, 30/32 sync word bits
-        2, 0x13, 0x22,      // MDMCFG1     FEC disabled, 2 preamble bytes
-        2, 0x14, 0xF8,      // MDMCFG0     CHANSPC = 199.951 kHz
-        2, 0x0A, 0x01,      // CHANNR      Channel number 1
-        2, 0x15, 0x00,      // DEVIATN     MSK phase change duty cycle
-        2, 0x21, 0xB6,      // FREND1
-        2, 0x22, 0x10,      // FREND0
-        2, 0x18, 0x18,      // MCSM0       Auto calibrate, Poweron timeout
-        2, 0x19, 0x1D,      // FOCCFG
-        2, 0x1A, 0x1C,      // BSCFG
-        2, 0x1B, 0xC7,      // AGCTRL2
-        2, 0x1C, 0x00,      // AGCTRL1
-        2, 0x1D, 0xB0,      // AGCTRL0
-        2, 0x23, 0xEA,      // FSCAL3
-        2, 0x24, 0x0A,      // FSCAL2
-        2, 0x25, 0x00,      // FSCAL1
-        2, 0x26, 0x11,      // FSCAL0
-        2, 0x29, 0x59,      // FSTEST      Test only (default)
-        2, 0x2C, 0x88,      // TEST2       Test settings (default)
-        2, 0x2D, 0x31,      // TEST1       Test settings (default)
-        2, 0x2E, 0x0B,      // TEST0       Test settings (default)
-        2, 0x00, 0x29,      // IOCFG2      GDO2 used as CHIP_RDYn (default)
-        2, 0x02, 0x06,      // IOCFG0      GDO0 used as sync
-        2, 0x07, 0x00,      // PKTCTRL1    Addr check, broadcast, CRC autoflush
-        2, 0x08, 0x01,      // PKTCTRL0    No whitening, normal TX/RX mode, CRC on
-        2, 0x09, 0x01,      // ADDR        Device address 0x01
-        2, 0x06, 0x09,      // PKTLEN      Using 9-byte packet
-        9, 0x7E,            // PATABLE     Set maximum output power
+        1, 0x30,      // SRES        Soft reset strobe
+	2, 0x02, 0x06 // IOCFG0 - GDO0Output Pin Configuration 
+	2, 0x07, 0x00 // PKTCTRL1 - Packet Automation Control
+	2, 0x08, 0x02 // PKTCTRL0 - Packet Automation Control
+	2, 0x0A, 0x2A // CHANNR - Channel Number 
+	2, 0x0B, 0x0A // FSCTRL1 - Frequency Synthesizer Control 
+	2, 0x0D, 0x5d // FREQ2 - Frequency Control Word, High Byte 
+	2, 0x0E, 0x44 // FREQ1 - Frequency Control Word, Middle Byte 
+	2, 0x10, 0x1B // MDMCFG4 - Modem Configuration 
+	2, 0x11, 0x43 // MDMCFG3 - Modem Configuration 
+	2, 0x12, 0x7F // MDMCFG2 - Modem Configuration
+	2, 0x14, 0x47 // MDMCFG0 - Modem Configuration 
+	2, 0x15, 0x01 // DEVIATN - Modem Deviation Setting 
+	2, 0x18, 0x18 // MCSM0 - Main Radio Control State Machine Configuration 
+	2, 0x19, 0x1D // FOCCFG - Frequency Offset Compensation Configuration
+	2, 0x1A, 0x1C // BSCFG - Bit Synchronization Configuration
+	2, 0x1B, 0xC7 // AGCCTRL2 - AGC Control
+	2, 0x1C, 0x00 // AGCCTRL1 - AGC Control
+	2, 0x1D, 0xB0 // AGCCTRL0 - AGC Control
+	2, 0x21, 0xB6 // FREND1 - Front End RX Configuration 
+	2, 0x25, 0x00 // FSCAL1 - Frequency Synthesizer Calibration 
+	2, 0x26, 0x11 // FSCAL0 - Frequency Synthesizer Calibration
+	9, 0x7E,      // PATABLE     Set maximum output power
            0xFF, 0xFF, 0xFF, 0xFF,
            0xFF, 0xFF, 0xFF, 0xFF,
-        1, 0x3A,            // SFRX        strobe: flush RX fifo
-        1, 0x3B,            // SFTX        strobe: flush TX fifo
-        1, 0x34,            // SRX         Wait for packets
+        1, 0x3A,      // SFRX        strobe: flush RX fifo
+        1, 0x3B,      // SFTX        strobe: flush TX fifo
+        1, 0x34,      // SRX         Wait for packets
         0,
     };
     
--- haradio.h
+++ haradio.h
@@ -18,6 +18,10 @@ public:
     void reset();
     void txBuffer(const uint8_t *buffer, uint8_t len);
     uint8_t rxBuffer(uint8_t *buffer, uint8_t len);
+    
+    int HARadio::setup_packet(struct packet *p);
+    int HARadio::receive_packet(struct packet *p, uint8_t *buffer);
+    int HARadio::send_packet(struct packet *p);
 
 private:
 
